ecd69ccce7f4bd705caccbe551cedd7cc91abce9
Sathis <rsathisk15@gmail.com>
2025-07-04 21:23:35 +0200

    Account creation routes and controller added

--- controllers/accountController.js
+++ controllers/accountController.js
@@ -19,3 +19,63 @@ export const getAccounts = async (req, res) => {
       .json({ status: 'failed', error: error, message: error?.message });
   }
 };
+
+export const createAccount = async (req, res) => {
+  try {
+    const { userId, name, amount, account_number } = req.body;
+
+    const accountExistQuery = {
+      text: 'SELECT * FROM tblaccount WHERE account_name = $1 and user_id = $2',
+      values: [name, userId],
+    };
+
+    const accountExistResult = await pool.query(accountExistQuery);
+
+    const isAccountExist = accountExistResult.rows[0];
+
+    if (isAccountExist) {
+      return res
+        .status(409)
+        .json({ status: 'failed', message: 'Account already created' });
+    }
+
+    const createAccountResult = await pool.query({
+      text: 'INSERT INTO tblaccount (user_id, account_name, account_number, account_balance) VALUES ($1, $2, $3, $4) RETURNING *',
+      values: [userId, name, account_number, amount],
+    });
+
+    const account = createAccountResult.rows[0];
+
+    const userAccounts = Array.isArray(name) ? name : [name];
+
+    const updateUserAccountQuery = await pool.query({
+      text: 'UPDATE tbluser SET accounts = array_cat(accounts, $1), updatedat = CURRENT_TIMESTAMP WHERE id = $2',
+      values: [userAccounts, userId],
+    });
+
+    const description = account.account_name + ' (Initial Deposit)';
+
+    const initialDepositResult = await pool.query({
+      text: 'INSERT INTO tbltransaction(user_id, description, type, status, amount, source) VALUES ($1, $2, $3, $4, $5, $6) RETURNING *',
+      values: [
+        userId,
+        description,
+        'income',
+        'Completed',
+        amount,
+        account.account_name,
+      ],
+    });
+
+    res.status(201).json({
+      status: 'success',
+      message: account.account_name + ' Account created successfully',
+      data: account,
+    });
+  } catch (error) {
+    console.log(error);
+    res
+      .status(500)
+      .json({ status: 'failed', error: error, message: error?.message });
+  }
+};
--- routes/accountRoutes.js
+++ routes/accountRoutes.js
@@ -1,9 +1,13 @@
 import express from 'express';
 import { authMiddleWare } from '../middlewares/authMiddleWare.js';
-import { getAccounts } from '../controllers/accountController.js';
+import {
+  createAccount,
+  getAccounts,
+} from '../controllers/accountController.js';
 
 const router = express.Router();
 
 router.get('/', authMiddleWare, getAccounts);
+router.post('/create-account', authMiddleWare, createAccount);
 
 export default router;
