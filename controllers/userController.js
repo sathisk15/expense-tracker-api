ed922f5ae8aeb8a57ee06cb49be1952ca8c09a16
Sathis <rsathisk15@gmail.com>
2025-07-04 15:26:35 +0200

    Routes added for update password

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,4 +1,5 @@
 import { pool } from '../configs/db.js';
+import { hashPassword } from '../utils/utils.js';
 
 export const getUser = async (req, res) => {
   try {
@@ -21,6 +22,46 @@ export const getUser = async (req, res) => {
     return res
       .status(200)
       .json({ status: 'success', message: 'User found successfully', user });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ status: 'failed', message: error?.message });
+  }
+};
+
+export const updateUserPassword = async (req, res) => {
+  try {
+    const { userId, confirmPassword, newPassword } = req.body;
+
+    if (newPassword !== confirmPassword) {
+      return res
+        .status(400)
+        .json({ status: 'failed', message: 'Password is not same' });
+    }
+
+    const result = await pool.query({
+      text: 'SELECT * FROM tbluser WHERE id = $1',
+      values: [userId],
+    });
+
+    const user = result.rows[0];
+
+    if (!user) {
+      return res
+        .status(404)
+        .json({ status: 'failed', message: 'No user found !' });
+    }
+
+    const hashedPassword = await hashPassword(newPassword);
+
+    await pool.query({
+      text: `UPDATE tbluser SET password = $1 WHERE id = $2`,
+      values: [hashedPassword, userId],
+    });
+
+    return res.status(200).json({
+      status: 'success',
+      message: 'Password changed successfully',
+    });
   } catch (error) {
     console.log(error);
     res.status(500).json({ status: 'failed', message: error?.message });
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,9 +1,14 @@
 import express from 'express';
 import { authMiddleWare } from '../middlewares/authMiddleWare.js';
-import { getUser } from '../controllers/userController.js';
+import {
+  getUser,
+  updateUserPassword,
+  updateUserinfo,
+} from '../controllers/userController.js';
 
 const router = express.Router();
 
 router.get('/', authMiddleWare, getUser);
+router.put('/update-password', authMiddleWare, updateUserPassword);
 
 export default router;
