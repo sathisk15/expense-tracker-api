916b6b42d324a3ddc8451f91464091890ee0eba3
Sathis <rsathisk15@gmail.com>
2025-07-05 15:24:29 +0200

    Add Transactions and Transfer money Routes and controllers completed

--- controllers/transactionController.js
+++ controllers/transactionController.js
@@ -11,7 +11,7 @@ export const getTransactions = async (req, res) => {
     tomorrow.setDate(tomorrow.getDate() + 1);
     const sevenDaysAgo = new Date(tomorrow);
     sevenDaysAgo.setDate(tomorrow.getDate() - 7);
-    
+
     const startDate = formatDBDate(df || sevenDaysAgo);
     const endDate = formatDBDate(dt || tomorrow);
 
@@ -52,7 +52,145 @@ ORDER BY id DESC`,
 
 export const addTransaction = async (req, res) => {
   try {
-    const { userId, description, source, amount, type } = req.body;
+    const { userId, description, source, amount } = req.body;
+    const { account_id } = req.query;
+
+    if (!(description && source && amount)) {
+      return res.status(400).json({
+        status: 'failed',
+        message: 'Provide required Fields',
+      });
+    }
+
+    if (Number(amount) <= 0) {
+      return res.status(400).json({
+        status: 'failed',
+        message: 'Amount should be greater than 0 !',
+      });
+    }
+
+    const accountInfoResult = await pool.query({
+      text: `SELECT * FROM tblaccount WHERE id = $1`,
+      values: [account_id],
+    });
+
+    const accountInfo = accountInfoResult.rows[0];
+
+    if (!accountInfo) {
+      return res.status(404).json({
+        status: 'failed',
+        message: 'No Account !',
+      });
+    }
+
+    if (
+      accountInfo.account_balance <= 0 ||
+      accountInfo.account_balance < Number(amount)
+    ) {
+      return res.status(403).json({
+        status: 'failed',
+        message: 'Transaction failed. Insufficient funds !',
+      });
+    }
+    await pool.query('BEGIN');
+    await pool.query({
+      text: `UPDATE tblaccount SET account_balance = account_balance - $1, updatedat = CURRENT_TIMESTAMP WHERE id = $2`,
+      values: [amount, account_id],
+    });
+    await pool.query({
+      text: `INSERT INTO tbltransaction (user_id, description, type, status, amount, source) VALUES ($1, $2, $3, $4, $5, $6)`,
+      values: [userId, description, 'expense', 'Completed', amount, source],
+    });
+    await pool.query('COMMIT');
+
+    res.status(200).json({
+      status: 'success',
+      message: 'Transaction completed successfully',
+    });
+  } catch (error) {
+    res.status(500).json({
+      status: 'failed',
+      message: error?.message,
+    });
+  }
+};
+
+export const transerMoneyToAccount = async (req, res) => {
+  try {
+    const { userId, fromAccountId, toAccountId, amount } = req.body;
+
+    if (!(fromAccountId && toAccountId && amount)) {
+      return res.status(400).json({
+        status: 'failed',
+        message: 'Provide required Fields',
+      });
+    }
+
+    const newAmount = Number(amount);
+
+    if (Number(newAmount) <= 0) {
+      return res.status(400).json({
+        status: 'failed',
+        message: 'Amount should be greater than 0 !',
+      });
+    }
+
+    const fromAccountResult = await pool.query({
+      text: `SELECT * FROM tblaccount WHERE id = $1`,
+      values: [Number(fromAccountId)],
+    });
+
+    const fromAccount = fromAccountResult.rows[0];
+
+    if (newAmount > fromAccount.account_balance) {
+      return res.status(403).json({
+        status: 'failed',
+        message: 'Transer failed. Insufficient funds !',
+      });
+    }
+
+    await pool.query('BEGIN');
+    await pool.query({
+      text: `UPDATE tblaccount SET account_balance = account_balance - $1, updatedat = CURRENT_TIMESTAMP WHERE id = $2`,
+      values: [newAmount, fromAccountId],
+    });
+    const toAccount = await pool.query({
+      text: `UPDATE tblaccount SET account_balance = account_balance + $1, updatedat = CURRENT_TIMESTAMP WHERE id = $2 RETURNING *`,
+      values: [newAmount, toAccountId],
+    });
+
+    const transactionDetails = `(${fromAccount.account_name} - ${toAccount.rows[0].account_name})`;
+
+    await pool.query({
+      text: `INSERT INTO tbltransaction (user_id, description, type, status, amount, source) VALUES ($1, $2, $3, $4, $5, $6)`,
+      values: [
+        userId,
+        `Transfer ${transactionDetails}`,
+        'expense',
+        'Completed',
+        amount,
+        fromAccount.account_name,
+      ],
+    });
+
+    await pool.query({
+      text: `INSERT INTO tbltransaction (user_id, description, type, status, amount, source) VALUES ($1, $2, $3, $4, $5, $6)`,
+      values: [
+        userId,
+        `Received ${transactionDetails}`,
+        'income',
+        'Completed',
+        amount,
+        toAccount.rows[0].account_name,
+      ],
+    });
+
+    await pool.query('COMMIT');
+
+    res.status(201).json({
+      status: 'success',
+      message: 'Transfer completed successfully',
+    });
   } catch (error) {
     res.status(500).json({
       status: 'failed',
--- routes/transactionRoutes.js
+++ routes/transactionRoutes.js
@@ -1,9 +1,13 @@
 import express from 'express';
 import { authMiddleWare } from '../middlewares/authMiddleWare.js';
-import { getTransactions } from '../controllers/transactionController.js';
+import {
+  getTransactions,
+  transerMoneyToAccount,
+} from '../controllers/transactionController.js';
 
 const router = express.Router();
 
 router.get('/', authMiddleWare, getTransactions);
+router.post('/', authMiddleWare, transerMoneyToAccount);
 
 export default router;
